07eaba2b07fd1bd9145f860673e008846cdc02d6
nguyenhohoailinh_2120110318 <linhnguyen96789@gmail.com>
2023-03-27 15:06:46 +0700

    first commit

---
+++ WebsiteBanHang/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Context;
+
+namespace WebsiteBanHang.Controllers
+{
+
+   
+    public class HomeController : Controller
+    {
+        WebsiteBanHangEntities2 objWebsiteBanHangEntities2 = new WebsiteBanHangEntities2();
+        public ActionResult Index()
+        {
+            var lstCategory = objWebsiteBanHangEntities2.Categories.ToList();
+            return View(lstCategory);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
